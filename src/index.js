8582c4d2433842aa315ff643bc1189e9efc0060d
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-09-24 13:16:01 +0200

    Removes mongoose and user model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 
@@ -8,13 +7,3 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-
-mongoose
-  .connect(process.env.CONNECTION_STRING, { useUnifiedTopology: true })
-  .then((result) => {
-    console.log("Connected!");
-    app.listen(4000);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
