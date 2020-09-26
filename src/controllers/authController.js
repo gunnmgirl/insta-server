d3f6847a4db09df58df8a69d178a4220d7742a8a
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-09-26 16:03:28 +0200

    Adds validation to signup function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,19 +1,29 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { validationResult } from "express-validator/check";
 
 import User from "../models/userModel";
 
 async function signup(req, res, next) {
-  const {
-    firstName,
-    lastName,
-    username,
-    description,
-    profileImage,
-    email,
-    password,
-  } = req.body;
+  const errors = validationResult(req);
   try {
+    if (!errors.isEmpty()) {
+      const error = new Error();
+      error.statusCode = 422;
+      error.data = errors.array();
+      error.message = error.data[0].msg;
+      throw error;
+    }
+    const {
+      firstName,
+      lastName,
+      username,
+      description,
+      profileImage,
+      email,
+      password,
+    } = req.body;
+
     const hashedPassword = await bcrypt.hash(password, 12);
     const user = await User.create({
       firstName,
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,30 @@
 import express from "express";
+import { body } from "express-validator/check";
 
 import authController from "../controllers/authController";
+import User from "../models/userModel";
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
+router.post(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value) => {
+        return User.findOne({ where: { email: value } }).then((user) => {
+          if (user) {
+            return Promise.reject("Email already exists!");
+          }
+        });
+      }),
+    body("password")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("Password must be at least 3 characters long"),
+  ],
+  authController.signup
+);
 
 export default router;
