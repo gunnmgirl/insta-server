8421d1d5c2ab27b996b9d52e2cf53d64600fbe68
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-10-07 19:58:49 +0200

    Adds get post by id controller function

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -30,4 +30,34 @@ async function getFeedPosts(req, res, next) {
   }
 }
 
-export default { createPost, getFeedPosts };
+async function getAllPosts(req, res, next) {
+  const { page } = req.query;
+  try {
+    const posts = await Post.findAll({
+      raw: true,
+      offset: Number(page) * 10,
+      limit: 10,
+    });
+    res.status(200).send(posts);
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+}
+
+async function getPost(req, res, next) {
+  const { postId } = req.query;
+  try {
+    const post = await Post.findByPk(postId);
+    res.status(200).send(post.dataValues);
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+}
+
+export default { createPost, getFeedPosts, getAllPosts, getPost };
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post("/create", isAuth, postController.createPost);
 router.get("/feed", isAuth, postController.getFeedPosts);
+router.get("/explore", isAuth, postController.getFeedPosts);
+router.get("/post", isAuth, postController.getPost);
 
 export default router;
